5ca5959df93d775f32dbe691627b20591e333090
YossefYasser <yossef.yasser741@gmail.com>
2022-04-03 20:34:28 +0200

    feat: intialize firebase authentication

--- src/routes/v1/hello/hello.route.ts
+++ src/routes/v1/hello/hello.route.ts
@@ -1,9 +1,10 @@
 import { Router, Request, Response } from "express";
-
+import { verifyToken } from "../../../middleware";
 const router = Router({ mergeParams: true });
 
+router.use("/", verifyToken);
 router.route("/").get(function (req: Request, res: Response) {
-  res.status(200).send("Hello World!");
+  res.status(200).send("authorized");
 });
 
 export { router as helloRouter };
