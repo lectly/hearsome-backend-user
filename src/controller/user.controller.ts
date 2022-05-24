8b8ea9809d3b9bd880f1164ddf7e9d2bc4fadec4
YossefYasser <yossef.yasser741@gmail.com>
2022-05-24 04:38:24 +0200

    feat: update response of get singed url endpoint

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -4,8 +4,11 @@ import { getSignedURL } from "../utils";
 export class UserController {
   static async getSignedURL(req: Request, res: Response, next: NextFunction) {
     try {
-      const url = await getSignedURL();
-      return res.status(200).send(url);
+      const [url, key, bucket] = await getSignedURL();
+
+      return res
+        .status(200)
+        .json({ signedURL: url, objectName: key, bucketName: bucket });
     } catch (error) {
       return next(error);
     }
--- src/utils/getSignedURL.utils.ts
+++ src/utils/getSignedURL.utils.ts
@@ -1,25 +1,28 @@
+import "dotenv/config"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import * as uuid from "uuid";
-import { config } from "../config";
 
 const params = {
-  Bucket: config.AWS_S3_BUCKET_NAME,
-  Key: uuid.v4(),
-  expires: 200,
-  ContentType: "application/json",
+  Bucket: process.env.AWS_S3_BUCKET_NAME,
+  Key: "",
+  expires: 3600,
+  ContentType: "audio/aac",
 };
-
 const client = new S3Client({
   credentials: {
-    accessKeyId: config.AWS_KEY_ID,
-    secretAccessKey: config.AWS_SECRET_KEY,
+    accessKeyId: process.env.AWS_KEY_ID || "",
+    secretAccessKey: process.env.AWS_SECRET_KEY || "",
   },
-  region: config.AWS_REGION,
+  region: process.env.AWS_REGION || "",
 });
-
-const command = new PutObjectCommand(params);
-
+// "command" variable must be defined inside the getSignedURL function so the key variable(s3 object name) get a different value each time we access the endpoint
 export async function getSignedURL() {
-  return await getSignedUrl(client, command, { expiresIn: 3600 });
+  params.Key = uuid.v4(); // generate a unique string for the key each time
+  const command = new PutObjectCommand(params);
+  return [
+    await getSignedUrl(client, command, { expiresIn: 3600 }),
+    params.Key,
+    params.Bucket,
+  ];
 }
