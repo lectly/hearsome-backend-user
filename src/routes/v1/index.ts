6c0e3777078863c6673c039c2415d8b6375f850b
YossefYasser <yossef.yasser741@gmail.com>
2022-05-24 04:39:37 +0200

    refactor: delete uwnanted imports

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,11 +1,9 @@
 import express from "express";
 import { UserAuth } from "../../middleware";
-import { helloRouter } from "./hello";
 import { userRouter } from "./user";
 const router = express.Router();
 router.use("/", UserAuth.verifyToken);
 
-router.use("/hello", helloRouter);
 router.use("/user", userRouter);
 
 export { router as v1Router };
