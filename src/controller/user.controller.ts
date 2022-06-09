515acdb31d0b45f0c4d2d99fa4816bf1aaa7c6e8
Omar Adel <omarxadel21@gmail.com>
2022-06-09 17:53:30 +0200

    refactor: code cleanup before merge

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -4,11 +4,8 @@ import { getSignedURL } from "../utils";
 export class UserController {
   static async getSignedURL(req: Request, res: Response, next: NextFunction) {
     try {
-      const [url, key, bucket] = await getSignedURL();
-
-      return res
-        .status(200)
-        .json({ signedURL: url, objectName: key, bucketName: bucket });
+      const { signedURL, objectName, bucketName } = await getSignedURL();
+      return res.status(200).json({ signedURL, objectName, bucketName });
     } catch (error) {
       return next(error);
     }
--- src/utils/getSignedURL.utils.ts
+++ src/utils/getSignedURL.utils.ts
@@ -3,12 +3,8 @@ import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import * as uuid from "uuid";
 
-const params = {
-  Bucket: process.env.AWS_S3_BUCKET_NAME,
-  Key: "",
-  expires: 3600,
-  ContentType: "audio/aac",
-};
+const AWS_CREDENTIAL_EXPIRY = 3600; // 3600 seconds
+
 const client = new S3Client({
   credentials: {
     accessKeyId: process.env.AWS_KEY_ID || "",
@@ -16,13 +12,21 @@ const client = new S3Client({
   },
   region: process.env.AWS_REGION || "",
 });
-// "command" variable must be defined inside the getSignedURL function so the key variable(s3 object name) get a different value each time we access the endpoint
+
 export async function getSignedURL() {
-  params.Key = uuid.v4(); // generate a unique string for the key each time
+  const params = {
+    Bucket: process.env.AWS_S3_BUCKET_NAME || "",
+    Key: uuid.v4(),
+    expires: AWS_CREDENTIAL_EXPIRY,
+    ContentType: "audio/aac",
+  };
+
   const command = new PutObjectCommand(params);
-  return [
-    await getSignedUrl(client, command, { expiresIn: 3600 }),
-    params.Key,
-    params.Bucket,
-  ];
+  return {
+    signedURL: await getSignedUrl(client, command, {
+      expiresIn: AWS_CREDENTIAL_EXPIRY,
+    }),
+    objectName: params.Key,
+    bucketName: params.Bucket,
+  };
 }
