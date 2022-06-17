2ddf410e30c87b8403164bee13687c1b7790faae
Omar Adel <omarxadel21@gmail.com>
2022-06-17 18:40:25 +0200

    refactor: renamed controllers dir

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,10 +1,10 @@
 import { Request, Response, NextFunction } from "express";
-import { getSignedURL } from "../utils";
+import { S3 } from "../aws";
 
 export class UserController {
   static async getSignedURL(req: Request, res: Response, next: NextFunction) {
     try {
-      const { signedURL, objectName, bucketName } = await getSignedURL();
+      const { signedURL, objectName, bucketName } = await S3.getSignedURL();
       return res.status(200).json({ signedURL, objectName, bucketName });
     } catch (error) {
       return next(error);
