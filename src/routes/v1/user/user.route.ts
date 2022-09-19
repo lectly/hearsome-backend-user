f9194ac153843890c79f057961732ff89aa12ef4
Omar Adel <omarxadel21@gmail.com>
2022-09-19 04:01:50 +0200

    feat: added mock transcribe endpoint

--- src/routes/v1/user/user.route.ts
+++ src/routes/v1/user/user.route.ts
@@ -4,5 +4,6 @@ import { UserController } from "../../../controllers";
 const router = Router({ mergeParams: true });
 
 router.use("/signed_url", UserController.getSignedURL);
+router.use("/transcribe", UserController.transcribe);
 
 export { router as userRouter };
